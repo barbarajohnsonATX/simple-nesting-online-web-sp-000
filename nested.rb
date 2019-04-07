45e5d21549c2f77160279f3c581ebca1948baa42
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 04:18:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,8 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  #programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-  
+  programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP", "C"], :known_for => "Ruby"}
+   
      
 end
 
